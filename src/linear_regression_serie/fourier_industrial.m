937be23929cb03a18ee4438bc374715a2fe9a233
GiulioP23 <giulio.presti01@universitadipavia.it>
2025-01-21 14:54:04 +0100

    rename

---
+++ src/linear_regression_serie/fourier_industrial.m
@@ -0,0 +1,35 @@
+%% 1. Startup
+clear 
+clc
+close all
+
+%% Data selection and analysis
+df = caricaLoad(); % nota: la funzione deve essere aggiunta al path di matlab
+locations = unique(df.Location_Electricity(df.Location_Electricity.endsWith('i')));
+quarters = unique(df.Quarter);
+
+for n=1:length(locations)
+    location = locations(n);    
+
+    locData = df(df.Location_Electricity==location, :);
+    for q=1:4
+        quarter = quarters(q);        
+        qData = locData(locData.Quarter==quarter, :);
+        [mse, params] = fourier_tests_1d(qData, sprintf("Location %s (%s)", location.replace('_', ''), quarter), false, false);
+        fprintf("Location %s, Quarter %s\n\tFPE-nparam: %d\n\tFPE-MSE: %3.2f\n", location, quarter, params('FPE'), mse('FPE'));
+    end
+    
+end
+
+%% Test a specific location and period
+% In questa sezione è possibile visualizzare i risultati dei test per una
+% specifica serie selezionata di seguito, e i relativi grafici.
+
+quarter = "Q3";
+location = "2_i";
+
+locData = df(df.Location_Electricity==location, :);
+qData = locData(locData.Quarter==quarter, :);
+[mse, params] = fourier_tests_1d(qData, sprintf("Location %s (%s)", location.replace('_', ''), quarter), true, true);   
+
+fprintf("Quarter %s, Location\nOrdine:%s\n\tF: %d\n\tFPE: %d\n\tAIC: %d\n\tMDL: %d\n\tCrossvalidazione: %d\n", quarter, location, params("F"), params("FPE"), params("AIC"), params("MDL"), params("CROSS"))
--- src/linear_regression_serie/polinomial_industrial.m
+++ src/linear_regression_serie/polinomial_industrial.m
@@ -5,7 +5,7 @@ close all
 
 %% Data selection and analysis
 df = caricaLoad(); % nota: la funzione deve essere aggiunta al path di matlab
-locations = unique(df.Location_Electricity(df.Location_Electricity.endsWith('i')));
+locations = unique(df.Location_Electricity(df.Location_Electricity.endsWith('r')));
 quarters = unique(df.Quarter);
 
 for n=1:length(locations)
@@ -15,18 +15,21 @@ for n=1:length(locations)
     for q=1:4
         quarter = quarters(q);        
         qData = locData(locData.Quarter==quarter, :);
-        [mse, params] = fourier_tests_1d(qData, sprintf("Location %s (%s)", location.replace('_', ''), quarter), false, false);
+        [mse, params] = polinomial_tests_1d(qData, sprintf("Location %s (%s)", location.replace('_', ''), quarter), false, false);
         fprintf("Location %s, Quarter %s\n\tFPE-nparam: %d\n\tFPE-MSE: %3.2f\n", location, quarter, params('FPE'), mse('FPE'));
     end
     
 end
 
 %% Test a specific location and period
-quarter = "Q3";
+% In questa sezione è possibile visualizzare i risultati dei test per una
+% specifica serie selezionata di seguito, e i relativi grafici.
+
+quarter = "Q2";
 location = "2_i";
 
 locData = df(df.Location_Electricity==location, :);
 qData = locData(locData.Quarter==quarter, :);
-[mse, params] = fourier_tests_1d(qData, sprintf("Location %s (%s)", location.replace('_', ''), quarter), true, true);   
+[mse, params] = polinomial_tests_1d(qData, sprintf("Location %s (%s)", location.replace('_', ''), quarter), true, true);   
 
 fprintf("Quarter %s, Location\nOrdine:%s\n\tF: %d\n\tFPE: %d\n\tAIC: %d\n\tMDL: %d\n\tCrossvalidazione: %d\n", quarter, location, params("F"), params("FPE"), params("AIC"), params("MDL"), params("CROSS"))
